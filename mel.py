f4b3090f4f010888c52c20f391fd89c68292a47b
nurmeldayanti1167 <nurmeldayanti58@gmail.com>
2026-04-16 08:35:21 +0800

    commit 5

--- mel.py
+++ mel.py
@@ -8,3 +8,7 @@ potongan = harga * (persen / 100)
 total = harga - potongan
 print(f"Potongan: Rp{potongan:,.2f}")
 print(f"Total Bayar: Rp{total:,.2f}")
+try:
+    # Jalankan semua kode di atas
+except ValueError:
+    print("Input harus angka!")
