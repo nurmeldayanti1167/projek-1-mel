c46c8cb2547a9ef62b7b1c4954292bf6191338ee
nurmeldayanti1167 <nurmeldayanti58@gmail.com>
2026-04-16 08:32:43 +0800

    commit 4

--- mel.py
+++ mel.py
@@ -6,3 +6,5 @@ hitung()
 persen = float(input("Diskon (%): "))
 potongan = harga * (persen / 100)
 total = harga - potongan
+print(f"Potongan: Rp{potongan:,.2f}")
+print(f"Total Bayar: Rp{total:,.2f}")
